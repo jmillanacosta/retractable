f92606361d3ee3f3fc811672250e992bfd5d5331
jmillanacosta <javier.millan.acosta@gmail.com>
2023-08-02 10:54:05 +0200

    More error handling

--- src/main/get_epmc.py
+++ src/main/get_epmc.py
@@ -41,7 +41,7 @@ def get_retracted_articles_epmc(query_url, article_url):
         query_url (str): The EuropePMC API URL for searching
         article_url (str): The EuropePMC API URL for an article
     """
-    url=query_url
+    url = query_url
     global cursor_mark
     # Initialize the list to store retracted articles
     retracted_articles_ePMC = []
@@ -60,48 +60,51 @@ def get_retracted_articles_epmc(query_url, article_url):
 
     # Calculate the number of requests needed to retrieve all results
     num_requests = (total + page_size) // page_size + 1
-    print(f"Will need to perform {num_requests+1} requests.")
-    j =0
+    print(f"Will need to perform {num_requests + 1} requests.")
+    j = 0
     # Iterate through each page and append the results to the list
-    for i in range(num_requests): 
+    for i in range(num_requests):
         cursor_mark = dict_response.get('nextCursorMark')
         if cursor_mark is None:
             break
         url = query_url.format(cursor_mark, page_size, format_type)
-        print(f"\nRequest {i+1} of {num_requests+1}: {url}")
+        print(f"\nRequest {i + 1} of {num_requests + 1}: {url}")
         response = make_request(url)
         if response is None:
             continue
         o = xmltodict.parse(response.text)
         dict_response = o['responseWrapper']
-        result = dict_response.get('rdf:RDF', {}).get('rdf:Description',{})
-        j +=1
+        result = dict_response.get('rdf:RDF', {}).get('rdf:Description', {})
+        j += 1
 
         for item in result:
-
-            # Drop abstract
-            if 'dcterms:abstract' in item.keys():
-                item.pop('dcterms:abstract')
-            print('Cummmulative of retracted papers found:', j, end='\r',flush=True)
-            j+=1
-            # Get retraction notice  
-            source = item['@rdf:about'].split('/', 4)[-1].split('/')[0]
-            id = item['@rdf:about'].split('/', 4)[-1].split('/')[1]
-            article_url_i = article_url.format(source, id, 'json')
-            article_retract = make_request(article_url_i).json().get('result', {}).get("commentCorrectionList", {}).get("commentCorrection", [])[0]
-            retract_id = article_retract.get('id', 'NA')
-            retract_source = article_retract.get('source', 'NA')
-            retraction_url = article_url.format(retract_source, retract_id, 'dc')
-            retraction_json = xmltodict.parse(make_request(retraction_url).text)['responseWrapper']
             try:
-                item['retraction'] = retraction_json.get('rdf:RDF', {}).get('rdf:Description',{})
-            except:
-                print('Skipping', retract_id)
+                # Drop abstract
+                if 'dcterms:abstract' in item.keys():
+                    item.pop('dcterms:abstract')
+                print('Retracted papers:', j, end='\r', flush=True)
+                j += 1
+                # Get retraction notice
+                source = item['@rdf:about'].split('/', 4)[-1].split('/')[0]
+                id = item['@rdf:about'].split('/', 4)[-1].split('/')[1]
+                article_url_i = article_url.format(source, id, 'json')
+                article_retract = make_request(article_url_i).json().get('result', {}).get("commentCorrectionList", {}).get("commentCorrection", [])[0]
+                retract_id = article_retract.get('id', 'NA')
+                retract_source = article_retract.get('source', 'NA')
+                retraction_url = article_url.format(retract_source, retract_id, 'dc')
+                retraction_json = xmltodict.parse(make_request(retraction_url).text)['responseWrapper']
+                try:
+                    item['retraction'] = retraction_json.get('rdf:RDF', {}).get('rdf:Description', {})
+                except Exception as e:
+                    print(f"No retraction for {retract_id}: {e}")
+                    continue
+                if 'dcterms:abstract' in item['retraction'].keys():
+                    item['retraction'].pop('dcterms:abstract')
+                if 'retraction' in item['retraction'].keys():
+                    item['retraction'].pop('retraction')
+                retracted_articles_ePMC.extend([item])
+            except (IndexError, KeyError) as e:
+                print(f"Skipping item due to IndexError/KeyError: {e}")
                 continue
-            if 'dcterms:abstract' in item['retraction'].keys():
-                item['retraction'].pop('dcterms:abstract')
-            if 'retraction' in item['retraction'].keys():
-                item['retraction'].pop('retraction')
-            retracted_articles_ePMC.extend([item])
 
     return retracted_articles_ePMC
