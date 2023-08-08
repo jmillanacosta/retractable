de9164f7bca47f96bd467abb3f097e19eeee71e1
Javier Millán Acosta <83466805+jmillanacosta@users.noreply.github.com>
2023-08-08 15:49:57 +0200

    attempt to optimize

--- src/main/get_epmc.py
+++ src/main/get_epmc.py
@@ -1,30 +1,17 @@
 #!/usr/bin/env python3
-import xmltodict, json
+import xmltodict
 import pandas as pd
-import json
-import yaml
-import numpy as np
 import requests
-import re
+from concurrent.futures import ThreadPoolExecutor
 
 USER_AGENT = "Mozilla/5.0"
 page_size = 1000
-cursor_mark = '*'
 format_type = 'dc'
 
 def make_request(url):
-    """
-    Make an HTTP GET request to the given URL and return the response.
-
-    Args:
-        url (str): The URL to make the request to.
-
-    Returns:
-        response: the response object
-    """
     try:
         response = requests.get(url)
-        response.raise_for_status()  # Raise exception for non-200 status codes
+        response.raise_for_status()
         return response
     except requests.exceptions.RequestException as e:
         print(f"An error occurred while making the request: {e}")
@@ -33,23 +20,29 @@ def make_request(url):
         print(f"An unexpected error occurred: {e}")
         return None
 
-def get_retracted_articles_epmc(query_url, article_url):
-    """
-    Get retracted articles from Europe PMC and save them to files.
+def get_retraction_info(article_url, source, id):
+    try:
+        article_url_i = article_url.format(source, id, 'json')
+        article_retract = make_request(article_url_i).json().get('result', {}).get("commentCorrectionList", {}).get("commentCorrection", [])[0]
+        retract_id = article_retract.get('id', 'NA')
+        retract_source = article_retract.get('source', 'NA')
+        retraction_url = article_url.format(retract_source, retract_id, 'dc')
+        retraction_json = xmltodict.parse(make_request(retraction_url).text)['responseWrapper']
 
-    Args:
-        query_url (str): The Europepmc API URL for searching
-        article_url (str): The Europepmc API URL for an article
-    """
+        rdf_retract = retraction_json.get('rdf:RDF', None)
+        if rdf_retract is not None:
+            retraction = rdf_retract.get('rdf:Description', None)
+            if retraction is not None:
+                return retraction
+    except Exception as e:
+        print(f"No retraction for {retract_id}: {e}")
+    return None
+
+def get_retracted_articles_epmc(query_url, article_url):
     url = query_url
     global cursor_mark
-    # Initialize the list to store retracted articles
     retracted_articles_epmc = []
 
-    print("Performing an initial request to Europe PMC to get the list of retracted papers")
-    # Make the initial request to get the total number of results
-    url = query_url.format(cursor_mark, "1", format_type)
-    print(url)
     response = make_request(url)
     if response is None:
         raise requests.exceptions.InvalidURL
@@ -57,71 +50,39 @@ def get_retracted_articles_epmc(query_url, article_url):
     dict_response = o['responseWrapper']
 
     total = int(dict_response.get('hitCount', 0))
-
-    # Calculate the number of requests needed to retrieve all results
     num_requests = (total + page_size) // page_size + 1
-    print(f"Will need to perform {num_requests + 1} requests.")
-    j = 0
-    # Iterate through each page and append the results to the list
-    for i in range(num_requests):
-        cursor_mark = dict_response.get('nextCursorMark')
-        if cursor_mark is None:
-            break
-        url = query_url.format(cursor_mark, page_size, format_type)
-        print(f"\nRequest {i + 1} of {num_requests + 1}: {url}")
-        response = make_request(url)
-        if response is None:
-            continue
-        o = xmltodict.parse(response.text)
-        dict_response = o['responseWrapper']
-        try:
-            result = dict_response.get('rdf:RDF', {}).get('rdf:Description', {})
-        except Exception as e:
-            print(f"Skipping item due to {e}")
-        j += 1
 
-        for item in result:
-            try:
-                # Drop abstract
-                if 'dcterms:abstract' in item.keys():
-                    item.pop('dcterms:abstract')
-                print('Retracted papers:', j, end='\r', flush=True)
-                j += 1
-                # Get retraction notice
-                source = item['@rdf:about'].split('/', 4)[-1].split('/')[0]
-                id = item['@rdf:about'].split('/', 4)[-1].split('/')[1]
-                article_url_i = article_url.format(source, id, 'json')
-                article_retract = make_request(article_url_i).json().get('result', {}).get("commentCorrectionList", {}).get("commentCorrection", [])[0]
-                retract_id = article_retract.get('id', 'NA')
-                retract_source = article_retract.get('source', 'NA')
-                retraction_url = article_url.format(retract_source, retract_id, 'dc')
+    with ThreadPoolExecutor() as executor:
+        futures = []
+
+        for i in range(num_requests):
+            cursor_mark = dict_response.get('nextCursorMark')
+            if cursor_mark is None:
+                break
+            url = query_url.format(cursor_mark, page_size, format_type)
+            print(f"\nRequest {i + 1} of {num_requests + 1}: {url}")
+            response = make_request(url)
+            if response is None:
+                continue
+            o = xmltodict.parse(response.text)
+            dict_response = o['responseWrapper']
+            result = dict_response.get('rdf:RDF', {}).get('rdf:Description', [])
+
+            for item in result:
                 try:
-                    retraction_json = xmltodict.parse(make_request(retraction_url).text)['responseWrapper']
+                    if 'dcterms:abstract' in item.keys():
+                        item.pop('dcterms:abstract')
+                    
+                    source = item['@rdf:about'].split('/', 4)[-1].split('/')[0]
+                    id = item['@rdf:about'].split('/', 4)[-1].split('/')[1]
+                    futures.append(executor.submit(get_retraction_info, article_url, source, id))
                 except Exception as e:
                     print(f"Skipping item due to {e}")
-                try:
-                    rdf_retract = retraction_json.get('rdf:RDF', None)
-                    if rdf_retract is not None:
-                        retraction = rdf_retract.get('rdf:Description', None)
-                        if retraction is not None:
-                            item[retraction] = retraction
-                        else:
-                            print("No retraction found")
-                except Exception as e:
-                    print(f"No retraction for {retract_id}: {e}")
-                    continue
-                try:
-                    if 'dcterms:abstract' in item['retraction'].keys():
-                        item['retraction'].pop('dcterms:abstract')
-                    if 'retraction' in item['retraction'].keys():
-                        item['retraction'].pop('retraction')
-                    retracted_articles_epmc.extend([item])
-                except Exception as e:
-                    print(f'Skipping item due to {e}')
-                    
-            except Exception as e:
-                print(f"Skipping item due to {e}")
-                continue
 
+        for future in futures:
+            retraction = future.result()
+            if retraction:
+                item['retraction'] = retraction
+                retracted_articles_epmc.append(item)
 
     return retracted_articles_epmc
